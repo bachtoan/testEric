4f7ac7c9e11727fc9b8447d2f5e1aeda7bd88086
BachToan <toanbui765@gmail.com>
2024-11-07 17:18:05 +0700

    styles

--- src/screens/DetailFeed/styles.ts
+++ src/screens/DetailFeed/styles.ts
@@ -1,4 +1,22 @@
-import ListReel from '@/components/reelList';
-import {StyleSheet} from 'react-native';
+import {createStyleSheet} from 'react-native-unistyles';
 
-export const styles = StyleSheet.create({});
+export const stylesheet = createStyleSheet(theme => ({
+  container: (top, bottom) => ({
+    flex: 1,
+    paddingTop: top,
+    paddingBottom: bottom,
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+  }),
+
+  contentView: {
+    paddingTop: 10,
+    paddingHorizontal: 16,
+  },
+
+  commentView: {
+    borderTopWidth: 1,
+    borderTopColor: '#F9FAFB',
+    padding: 10,
+  },
+}));
